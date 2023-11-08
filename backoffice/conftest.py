681e84c33b5d526bb184d0bb577f33451bcdcbfe
MJedr <jedrych.marcysia@gmail.com>
2023-11-08 16:57:11 +0100

    add migration to init groups

--- backoffice/conftest.py
+++ backoffice/conftest.py
@@ -1,4 +1,6 @@
 import pytest
+from django.db import connection
+from django.db.migrations.executor import MigrationExecutor
 
 from backoffice.users.models import User
 from backoffice.users.tests.factories import UserFactory
@@ -12,3 +14,19 @@ def media_storage(settings, tmpdir):
 @pytest.fixture
 def user(db) -> User:
     return UserFactory()
+
+
+class Migrator:
+    def __init__(self, connection=connection):
+        self.executor = MigrationExecutor(connection)
+
+    def migrate(self, app_label: str, migration: str):
+        target = [(app_label, migration)]
+        self.executor.loader.build_graph()
+        self.executor.migrate(target)
+        self.apps = self.executor.loader.project_state(target).apps
+
+
+@pytest.fixture
+def migrator():
+    return Migrator
