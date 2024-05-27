91a40470c4be474ccb4a9fe74db86aa53b095fdb
DonHaul <ramiro.animus@gmail.com>
2024-05-27 14:08:15 +0100

    fixing github workflows

--- config/urls.py
+++ config/urls.py
@@ -37,7 +37,7 @@ urlpatterns += [
         SpectacularSwaggerView.as_view(url_name="api-schema"),
         name="api-docs",
     ),
-    path('', include('submission.urls')),  # Include the app's URLs
+    path('', include('backoffice.submission.urls')),  # Include the app's URLs
 ]
 
 if settings.DEBUG:
